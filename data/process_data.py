1930eca06ddc42ceec98efb9b73e9edd4a08af47
Douglas Lima Fonseca <douglaslfti@gmail.com>
2022-11-23 22:45:22 -0300

    Creating the clean_data function

--- data/process_data.py
+++ data/process_data.py
@@ -28,7 +28,52 @@ def load_data(messages_filepath, categories_filepath):
     return df
 
 def clean_data(df):
-    pass
+  '''
+  OBJECTIVE:
+  This function is used to process the data. It removes duplicate data, renames the columns for better understanding, and corrects the data to be binarized.
+  
+  INPUT:
+  df - DataFrame containing the data.
+  
+  OUTPUT:
+  df - Returns the DataFrame with the processed data.
+  '''
+  # Create a dataframe of the 36 individual category columns
+  categories = df['categories'].str.split(';', expand=True)
+  categories.head()
+
+  # Select the first row of the categories dataframe
+  row = categories.iloc[0]
+
+  # Use this row to extract a list of new column names for categories.
+  # Removes the two last characters
+  category_colnames = list([item[0:-2] for item in row])
+
+  # Rename the columns of `categories`
+  categories.columns = category_colnames
+
+  for column in categories:
+    # Set each value to be the last character of the string
+    categories[column] = categories[column].str.replace(column+'-', '')
+    
+    # Convert column from string to numeric
+    categories[column] = categories[column].astype(int)
+
+  # In the related column replace the value 2 with 1
+  categories['related'] = categories['related'].replace(2, 1)
+
+  # Drop the original categories column from `df`
+  df.drop('categories',inplace=True, axis=1)
+
+  # Concatenate the original dataframe with the new `categories` dataframe
+  df = pd.concat([df, categories], sort=False, axis=1)
+
+  # Check number of duplicates
+  if len(df[df.duplicated()]) != 0:
+    # Drop duplicates
+    df.drop_duplicates(keep=False, inplace=True)
+
+  return df
 
 
 def save_data(df, database_filename):
